3b04cbc863baf345583201764d307178f76d7b63
jaxa-TS <jaxa-TS@users.noreply.github.com>
2021-01-11 16:44:48 +0900

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,6 +38,16 @@ test "password should have a minimum length" do
  test "authenticated? should return false for a user with nil digest" do
   assert_not @user.authenticated?(:remember, '')
 end
+test "order should be most recent first" do
+  assert_equal microposts(:most_recent), Micropost.first
+end
+test "associated microposts should be destroyed" do
+  @user.save
+  @user.microposts.create!(content: "Lorem ipsum")
+  assert_difference 'Micropost.count', -1 do
+    @user.destroy
+  end
+end
 end
   # test "the truth" do
   #   assert true
